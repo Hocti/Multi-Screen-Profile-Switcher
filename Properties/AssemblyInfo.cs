feaba49c7462d8bbb813f8407460de325bcee3d4
Hocti <hocti@hotmail.com>
2023-11-03 16:12:53 +0800

    changed some text

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("screenTray")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Multi-Screen Profile Switcher")]
+[assembly: AssemblyDescription("This is a portable tray application for Windows that allows users to save, remove, and switch between multiple screen profiles with a single click. Each profile includes settings for resolution, position, orientation, and refresh rate, making it an essential tool for anyone who uses multi-monitor setups.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("screenTray")]
+[assembly: AssemblyProduct("Multi-Screen Profile Switcher")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
